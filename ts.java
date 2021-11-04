6b68e224a22632a0e8d90f8585ea0d4d61eabd6c
Ilari Suhonen <52505120+bluelhf@users.noreply.github.com>
2021-11-04 13:48:19 +0200

    Tee ts.javasta täysin mahdoton lukea

--- ts.java
+++ ts.java
@@ -1,11 +1,1 @@
-import sun.misc.Unsafe;
-import java.lang.reflect.Field;
-
-public class ts {
-	public static void main(String[] args) throws Exception {
-		Field f = Unsafe.class.getDeclaredField("theUnsafe");
-		f.trySetAccessible();
-		Unsafe unsafe = (Unsafe) f.get(null);
-		unsafe.putByte(0, (byte) 255);
-	}
-}
+import sun.misc.Unsafe;class ts{public static void main(String[]args) throws Exception {var f=Unsafe.class.getDeclaredField("theUnsafe");f.trySetAccessible();((Unsafe)f.get(null)).putInt(0, 0);}}
